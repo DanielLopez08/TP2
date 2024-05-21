5d6efed26e7d4812cc5b982814dc23bbbfb4272d
SamDeb213 <samydebbah213@gmail.com>
2024-05-21 15:02:32 -0400

    script powershell git CLI

---
+++ git CLI.ps1
@@ -0,0 +1,7 @@
+﻿git init
+git add 5555.py
+git commit -m "Voici le projet"
+git remote add origin https://github.com/DanielLopez08/TP2.git
+git push --set-upstream origin master
+git clone https://github.com/DanielLopez08/TP2.git
+git pull
