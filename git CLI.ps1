d2c81fa240fad03be4e46300ad1bf0ebe1f8ab20
SamDeb213 <samydebbah213@gmail.com>
2024-05-21 21:46:27 -0400

    Update git CLI.ps1

--- git CLI.ps1
+++ git CLI.ps1
@@ -1,4 +1,13 @@
-﻿git init
+#L’utilisation de git en ligne de commande et de GitHub
+#Samy Debbah et Daniel Lopez
+#    e1960719 et eYYYYYY
+#         21-05-2024
+
+ 
+ 
+ 
+ 
+git init
 git add 5555.py
 git commit -m "Voici le projet"
 git remote add origin https://github.com/DanielLopez08/TP2.git
